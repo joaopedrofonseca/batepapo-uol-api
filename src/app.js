b9db4e8b5a5514b9faf833a52600291f807cf22b
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-01-15 17:32:58 -0300

    feat: post messages

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ server.post("/participants", async (req, res) => {
     const validation = participantSchema.validate({ name })
 
     if (validation.error) {
-        const errors = validation.error.details.map((detail) => detail.messsage)
+        const errors = validation.error.details.map((detail) => detail.message)
         return res.status(422).send(errors)
     }
 
@@ -69,6 +69,29 @@ server.get("/participants", async (req, res) => {
 })
 
 //endpoint MESSAGES
+server.post("/messages", async (req,res) => {
+    const user = req.headers.user
+    const {to , text, type} = req.body
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.any().valid('message', 'private_message')
+    })
+    const validation = messageSchema.validate({to, text, type})
+
+    if (validation.error){
+        const errors = validation.error.details.map((detail) => detail.message)
+        return res.status(422).send(errors)
+    }
+    try{
+        await db.collection("messages").insertOne(
+            { from: user, to, text, type, time: dayjs().format('HH:mm:ss')}
+        )
+        res.sendStatus(201)    
+    } catch(err){
+        res.sendStatus(500)
+    }
+})
 
 server.get("/messages", async (req,res) => {
     const { limit } = req.query
@@ -76,18 +99,21 @@ server.get("/messages", async (req,res) => {
 
     try{
         const messages = await db.collection("messages").find().toArray()
-        if(limit){
-            const filteredMessages = messages.filter((message, index) => index < limit)
-            return res.status(200).send(filteredMessages)
-        }
         if(user){
             const userMessages = messages.filter((m) => m.to === user || m.to === "Todos")
-            res.status(200).send(userMessages)    
+            return res.status(200).send(userMessages)    
         }
+        if (limit && user){
+            const userLimitMessages = messages.filter((m, i) => (m.to === user || m.to === "Todos") && i < limit )
+            return res.status(200).send(userLimitMessages)
+        }
+        console.log
     }catch(err){
         res.sendStatus(500)
     }
 })
+
+//endpoint STATUS
 
 server.listen(5000, () => {
     console.log("Servidor: http://localhost:5000")
